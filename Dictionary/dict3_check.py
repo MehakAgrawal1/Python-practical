41a016aca97620b00dc5b77588ee7e51a028a208
MehakAgrawal1 <106535818+MehakAgrawal1@users.noreply.github.com>
2022-05-30 20:43:39 +0530

    Update dict3_check.py

--- Dictionary/dict3_check.py
+++ Dictionary/dict3_check.py
@@ -1,4 +1,5 @@
 #Q3.Write a python program to Check for Key in Dictionary Value list.
+
 test_dict = {'Simply' : [{'CS' : 5}, {'Python' : 6}], 'for' : 2, 'CS' : 3} 
  
 print("The original dictionary is : " + str(test_dict)) 
