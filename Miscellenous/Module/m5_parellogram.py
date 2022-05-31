f02372b1f9901a489504939a772a9388e887d22a
MehakAgrawal1 <106535818+MehakAgrawal1@users.noreply.github.com>
2022-05-31 23:24:16 +0530

    calculate the area of a parellelogram

---
+++ Miscellenous/Module/m5_parellogram.py
@@ -0,0 +1,6 @@
+#Q5. Write a Python program to calculate the area of a parellelogram.
+
+base = float(input('Length of base: '))
+height = float(input('Measurement of height: '))
+area = base * height
+print("Area is: ", area)
